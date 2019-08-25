4ee244b002af587e608d6dd743ffb8986fb06f11
Lucas Sônego <sonego_lucas@outlook.com>
2019-08-25 11:28:08 -0300

    feat: lista de tarefas concluídas

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
 var elementoInput = document.getElementById('input')
-var elementoLista = document.getElementById('lista')
+var elementoListaTarefas = document.getElementById('listaTarefas')
+var elementoListaConcluidos = document.getElementById('listaConcluidos')
 
 var tarefas = JSON.parse(localStorage.getItem('tarefasStorage')) || [
     'Exemplo de tarefa'
+]
+
+var tarefasConcluidas = JSON.parse(localStorage.getItem('tarefasConcluidasStorage')) || [
+    'Tarefa Concluída'
 ]
 
 function adicionarTarefa(){
@@ -16,34 +21,70 @@ function adicionarTarefa(){
 }
 
 function renderTarefas(){
-    elementoLista.innerHTML = '';
+    elementoListaTarefas.innerHTML = '';
+    elementoListaConcluidos.innerHTML = '';
     for (tarefa of tarefas){
         var li = document.createElement('li');
         var texto = document.createTextNode(tarefa);
         var trashImg = document.createElement('img');
-        trashImg.setAttribute('src', 'src/trash.svg')
-        trashImg.setAttribute('height', '18')
+        trashImg.setAttribute('src', 'src/trash.svg');
+        trashImg.setAttribute('height', '18');
+
+        var checkImg = document.createElement('img');
+        checkImg.setAttribute('src', 'src/check.svg');
+        checkImg.setAttribute('height', '18');
 
         pos = tarefas.indexOf(tarefa);
-        trashImg.setAttribute('onclick', 'removeTarefa(' + pos + ')')
+        checkImg.setAttribute('onclick', 'tarefaConcluida(' + pos + ')');
+        trashImg.setAttribute('onclick', 'removeTarefa(' + pos + ', "tarefas")');
+
+        li.appendChild(texto);
+        li.appendChild(trashImg);
+        li.appendChild(checkImg);
+        li.setAttribute('class', 'tarefa');
+        elementoListaTarefas.appendChild(li);
+    }
+    for (tarefa of tarefasConcluidas){
+        var li = document.createElement('li');
+        var texto = document.createTextNode(tarefa);
+        var trashImg = document.createElement('img');
+        trashImg.setAttribute('src', 'src/trash.svg');
+        trashImg.setAttribute('height', '18');
+
+        pos = tarefas.indexOf(tarefa);
+        trashImg.setAttribute('onclick', 'removeTarefa(' + pos + ', "concluidas")');
 
         li.appendChild(texto);
         li.appendChild(trashImg);
         li.setAttribute('class', 'tarefa');
-        elementoLista.appendChild(li);
+        elementoListaConcluidos.appendChild(li);
     }
 }
 
 renderTarefas();
 
-function removeTarefa(pos){
+function removeTarefa(pos,lista){
+    if (lista == 'tarefas'){
+        tarefas.splice(pos,1);
+    }
+    if (lista == 'concluidas'){
+        tarefasConcluidas.splice(pos,1);   
+    }
+    renderTarefas();
+    salvarTarefas();
+}
+
+function tarefaConcluida(pos){
+    var tarefa = tarefas[pos];
     tarefas.splice(pos,1);
+    tarefasConcluidas.push(tarefa);
     renderTarefas();
     salvarTarefas();
 }
 
 function salvarTarefas(){
     localStorage.setItem('tarefasStorage', JSON.stringify(tarefas));
+    localStorage.setItem('tarefasConcluidasStorage', JSON.stringify(tarefasConcluidas));
 }
 
 elementoInput.addEventListener('keypress', function(event){
